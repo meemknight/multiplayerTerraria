668aab4e08c25250be7944b4ce064b29dad15d4d
meemknight <vladalex420@gmail.com>
2022-11-30 09:26:30 +0200

    first commit

---
+++ src/gameLayer/gameLayer.cpp
@@ -0,0 +1,63 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#include "gameLayer.h"
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include "platformInput.h"
+#include "imgui.h"
+#include <iostream>
+#include <sstream>
+#include "imfilebrowser.h"
+#include <gl2d/gl2d.h>
+
+
+struct GameData
+{
+
+
+}gameData;
+
+gl2d::Renderer2D renderer;
+
+bool initGame()
+{
+	gl2d::init();
+
+	renderer.create();
+
+	return true;
+}
+
+
+
+bool gameLogic(float deltaTime)
+{
+#pragma region init stuff
+	int w = 0; int h = 0;
+	w= platform::getWindowSizeX();
+	h = platform::getWindowSizeY();
+	
+	glViewport(0, 0, w, h);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	renderer.updateWindowMetrics(w, h);
+
+#pragma endregion
+
+
+	renderer.renderRectangle({100, 100, 100, 100}, Colors_Blue);
+		
+
+	renderer.flush();
+
+	return true;
+#pragma endregion
+
+}
+
+void closeGame()
+{
+
+	platform::writeEntireFile(RESOURCES_PATH "gameData.data", &gameData, sizeof(GameData));
+
+}
