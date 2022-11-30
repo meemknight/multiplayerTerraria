2dd5d1f80b12e38536aa2ffd6946e0de5edf4d0b
meemknight <vladalex420@gmail.com>
2022-11-30 13:00:29 +0200

    finished tiles bake more or less

--- src/gameLayer/map.cpp
+++ src/gameLayer/map.cpp
@@ -17,6 +17,14 @@ void Map::create(int x, int y)
 	safeGet(5, 3) = Tile::stone;
 	safeGet(6, 7) = Tile::stone;
 	
+	safeGet(3, 4) = Tile::dirt;
+	safeGet(4, 4) = Tile::dirt;
+	safeGet(4, 5) = Tile::dirt;
+	safeGet(4, 3) = Tile::dirt;
+	safeGet(4, 11) = Tile::dirt;
+	safeGet(5, 4) = Tile::dirt;
+	safeGet(6, 8) = Tile::dirt;
+
 	safeGet(8, 3) = Tile::stone;
 	safeGet(9, 3) = Tile::stone;
 	safeGet(10, 3) = Tile::stone;
@@ -63,17 +71,52 @@ void Map::bakeBlockUnsafe(int x, int y)
 	if (!t.isNone())
 	{
 
-
-		//bool leftTopAir = (x > 0 && y > 0 && unsafeGet(x - 1, y - 1).isAir());
+		bool leftTopAir = (x > 0 && y > 0 && unsafeGet(x - 1, y - 1).isAir());
 		bool leftMiddleAir = (x > 0 && unsafeGet(x - 1, y).isAir());
-		//bool leftBottomAir = (x > 0 && y < mapSize.y - 1 && unsafeGet(x - 1, y + 1).isAir());
+		bool leftBottomAir = (x > 0 && y < mapSize.y - 1 && unsafeGet(x - 1, y + 1).isAir());
 
-		//bool rightTopAir = (x < mapSize.x-1 && y > 0 && unsafeGet(x + 1, y - 1).isAir());
+		bool rightTopAir = (x < mapSize.x-1 && y > 0 && unsafeGet(x + 1, y - 1).isAir());
 		bool rightMiddleAir = (x < mapSize.x - 1 && unsafeGet(x + 1, y).isAir());
-		//bool rightBottomAir = (x < mapSize.x - 1 && y < mapSize.y - 1 && unsafeGet(x + 1, y + 1).isAir());
+		bool rightBottomAir = (x < mapSize.x - 1 && y < mapSize.y - 1 && unsafeGet(x + 1, y + 1).isAir());
 
 		bool topMiddleAir = (y > 0 && unsafeGet(x, y - 1).isAir());
 		bool bottomMiddleAir = (y < mapSize.y-1 && unsafeGet(x, y + 1).isAir());
+
+		bool leftTopDirt = 0;
+		bool leftMiddleDirt = 0;
+		bool leftBottomDirt = 0;
+		bool rightTopDirt = 0;
+		bool rightMiddleDirt = 0;
+		bool rightBottomDirt = 0;
+		bool topMiddleDirt = 0;
+		bool bottomMiddleDirt = 0;
+
+		//if (!t.isDirt())
+		{
+			leftTopDirt = (x > 0 && y > 0 && unsafeGet(x - 1, y - 1).isDirt());
+			leftMiddleDirt = (x > 0 && unsafeGet(x - 1, y).isDirt());
+			leftBottomDirt = (x > 0 && y < mapSize.y - 1 && unsafeGet(x - 1, y + 1).isDirt());
+
+			rightTopDirt = (x < mapSize.x - 1 && y > 0 && unsafeGet(x + 1, y - 1).isDirt());
+			rightMiddleDirt = (x < mapSize.x - 1 && unsafeGet(x + 1, y).isDirt());
+			rightBottomDirt = (x < mapSize.x - 1 && y < mapSize.y - 1 && unsafeGet(x + 1, y + 1).isDirt());
+
+			topMiddleDirt = (y > 0 && unsafeGet(x, y - 1).isDirt());
+			bottomMiddleDirt = (y < mapSize.y - 1 && unsafeGet(x, y + 1).isDirt());
+		}
+
+		if (t.isDirt()) 
+		{
+			leftTopDirt =		!leftTopDirt;
+			leftMiddleDirt =	!leftMiddleDirt;
+			leftBottomDirt =	!leftBottomDirt;
+			rightTopDirt =		!rightTopDirt;
+			rightMiddleDirt =	!rightMiddleDirt;
+			rightBottomDirt =	!rightBottomDirt;
+			topMiddleDirt =		!topMiddleDirt;
+			bottomMiddleDirt =	!bottomMiddleDirt;
+		}
+
 
 		if ( leftMiddleAir 
 			&& rightMiddleAir 
@@ -91,8 +134,16 @@ void Map::bakeBlockUnsafe(int x, int y)
 			)
 		{
 			//block fully surounded by air except left
-			t.variationX = 12;
-			t.variationY = 0;
+			if (leftMiddleDirt)
+			{
+				t.variationX = 0;
+				t.variationY = 13;
+			}
+			else
+			{
+				t.variationX = 12;
+				t.variationY = 0;
+			}
 		}
 		else if (
 			leftMiddleAir
@@ -101,8 +152,17 @@ void Map::bakeBlockUnsafe(int x, int y)
 			)
 		{
 			//block fully surounded by air except right
-			t.variationX = 9;
-			t.variationY = 0;
+
+			if (rightMiddleDirt)
+			{
+				t.variationX = 3;
+				t.variationY = 13;
+			}
+			else
+			{
+				t.variationX = 9;
+				t.variationY = 0;
+			}
 		}
 		else if (
 			leftMiddleAir
@@ -112,8 +172,18 @@ void Map::bakeBlockUnsafe(int x, int y)
 			)
 		{
 			//block fully surounded by air except top
-			t.variationX = 6;
-			t.variationY = 3;
+
+			if (topMiddleDirt)
+			{
+				t.variationX = 6;
+				t.variationY = 8;
+			}
+			else
+			{
+				t.variationX = 6;
+				t.variationY = 3;
+			}
+			
 		}
 		else if (
 			leftMiddleAir
@@ -123,8 +193,19 @@ void Map::bakeBlockUnsafe(int x, int y)
 			)
 		{
 			//block fully surounded by air except bottom
-			t.variationX = 6;
-			t.variationY = 0;
+			
+			if (bottomMiddleDirt)
+			{
+				t.variationX = 6;
+				t.variationY = 5;
+			}
+			else
+			{
+				t.variationX = 6;
+				t.variationY = 0;
+			}
+
+
 		}
 		else if (
 			topMiddleAir && bottomMiddleAir
@@ -133,8 +214,27 @@ void Map::bakeBlockUnsafe(int x, int y)
 			)
 		{
 			//block fully surounded by air except left right
-			t.variationX = 6;
-			t.variationY = 4;
+			if (leftMiddleDirt && rightMiddleDirt)
+			{
+				t.variationX = 9;
+				t.variationY = 11;
+			}
+			else if (leftMiddleDirt)
+			{
+				t.variationX = 0;
+				t.variationY = 14;
+			}
+			else if (rightMiddleDirt)
+			{
+				t.variationX = 3;
+				t.variationY = 14;
+			}
+			else
+			{
+				t.variationX = 6;
+				t.variationY = 4;
+			}
+			
 		}
 		else if (
 			!topMiddleAir && !bottomMiddleAir
@@ -143,8 +243,28 @@ void Map::bakeBlockUnsafe(int x, int y)
 			)
 		{
 			//block fully surounded by air except top bottom
-			t.variationX = 5;
-			t.variationY = 0;
+			if (topMiddleDirt && bottomMiddleDirt)
+			{
+				t.variationX = 6;
+				t.variationY = 12;
+			}
+			else if (topMiddleDirt)
+			{
+				t.variationX = 6;
+				t.variationY = 8;
+			}
+			else if (bottomMiddleDirt)
+			{
+				t.variationX = 6;
+				t.variationY = 5;
+			}
+			else
+			{
+				t.variationX = 5;
+				t.variationY = 0;
+			}
+
+			
 		}
 		else if (
 			leftMiddleAir
@@ -184,29 +304,221 @@ void Map::bakeBlockUnsafe(int x, int y)
 		}
 		else if (leftMiddleAir)
 		{
-			t.variationX = 0;
-			t.variationY = 0;
+			if (rightMiddleDirt)
+			{
+				t.variationX = 13;
+				t.variationY = 2;
+			}else
+			{
+				t.variationX = 0;
+				t.variationY = 0;
+			}
+			
 		}
 		else if (rightMiddleAir)
 		{
-			t.variationX = 4;
-			t.variationY = 0;
+			
+			if (leftMiddleDirt)
+			{
+				t.variationX = 13;
+				t.variationY = 3;
+			}
+			else
+			{
+				t.variationX = 4;
+				t.variationY = 0;
+			}
+
 		}
 		else if (topMiddleAir)
 		{
-			t.variationX = 1;
-			t.variationY = 0;
+			if(bottomMiddleDirt)
+			{
+				t.variationX = 13;
+				t.variationY = 0;
+			}
+			else
+			{
+				t.variationX = 1;
+				t.variationY = 0;
+			}
+			
 		}
 		else if (bottomMiddleAir)
 		{
-			t.variationX = 1;
-			t.variationY = 2;
+			if(topMiddleDirt)
+			{
+				t.variationX = 13;
+				t.variationY = 1;
+			}
+			else if (bottomMiddleDirt)
+			{
+				t.variationX = 1;
+				t.variationY = 2;
+			}
 		}
 		else
 		{
-			t.variationX = 1; //it is surounded by other blocks
-			t.variationY = 1;
+			//it is fully surounded by other blocks
+
+		
+
+			if (
+				leftMiddleDirt
+				&& rightMiddleDirt
+				&& topMiddleDirt
+				&& bottomMiddleDirt
+				)
+			{
+				//fully surounded by dirt
+				t.variationX = 6;
+				t.variationY = 11;
+			}
+			else if (!leftMiddleDirt
+				&& rightMiddleDirt
+				&& topMiddleDirt
+				&& bottomMiddleDirt
+				)
+			{
+				t.variationX = 12;
+				t.variationY = 8;
+			}
+			else if (leftMiddleDirt
+				&& !rightMiddleDirt
+				&& topMiddleDirt
+				&& bottomMiddleDirt
+				)
+			{
+				t.variationX = 12;
+				t.variationY = 5;
+			}
+			else if (leftMiddleDirt
+				&& rightMiddleDirt
+				&& !topMiddleDirt
+				&& bottomMiddleDirt
+				)
+			{
+				t.variationX = 11;
+				t.variationY = 8;
+			}
+			else if (leftMiddleDirt
+				&& rightMiddleDirt
+				&& topMiddleDirt
+				&& !bottomMiddleDirt
+				)
+			{
+				t.variationX = 11;
+				t.variationY = 5;
+			}
+			else if (!leftMiddleDirt
+				&& rightMiddleDirt
+				&& topMiddleDirt
+				&& !bottomMiddleDirt
+				)
+			{
+				//cadran 1
+				t.variationX = 3;
+				t.variationY = 5;
+			}
+			else if (leftMiddleDirt
+				&& !rightMiddleDirt
+				&& topMiddleDirt
+				&& !bottomMiddleDirt
+				)
+			{
+				//cadran 2
+				t.variationX = 2;
+				t.variationY = 5;
+			}
+			else if (leftMiddleDirt
+				&& !rightMiddleDirt
+				&& !topMiddleDirt
+				&& bottomMiddleDirt
+				)
+			{
+				//cadran 3
+				t.variationX = 2;
+				t.variationY = 6;
+			}
+			else if (!leftMiddleDirt
+				&& rightMiddleDirt
+				&& !topMiddleDirt
+				&& bottomMiddleDirt
+				)
+			{
+				//cadran 4
+				t.variationX = 3;
+				t.variationY = 6;
+			}
+			else if (!leftMiddleDirt
+				&& !rightMiddleDirt
+				&& topMiddleDirt
+				&& bottomMiddleDirt)
+			{
+				//top bottom dirt
+				t.variationX = 8;
+				t.variationY = 10;
+			}
+			else if (
+				leftMiddleDirt
+				&& rightMiddleDirt
+				&& !topMiddleDirt
+				&& !bottomMiddleDirt
+				)
+			{
+				//left right dirt
+				t.variationX = 10;
+				t.variationY = 7;
+			}
+			else if (leftMiddleDirt)
+			{
+				t.variationX = 10;
+				t.variationY = 7;
+			}else if (rightMiddleDirt)
+			{
+				t.variationX = 9;
+				t.variationY = 7;
+			}else if (topMiddleDirt)
+			{
+				t.variationX = 9;
+				t.variationY = 8;
+			}else if (bottomMiddleDirt)
+			{
+				t.variationX = 9;
+				t.variationY = 7;
+			}
+			else //no dirt
+			{
+				if (leftTopAir && rightTopAir)
+				{
+					t.variationX = 6;
+					t.variationY = 1;
+				}
+				else if (rightTopAir && rightBottomAir)
+				{
+					t.variationX = 11;
+					t.variationY = 0;
+				}
+				else if (leftTopAir && leftBottomAir)
+				{
+					t.variationX = 10;
+					t.variationY = 0;
+				}else if (leftBottomAir && rightBottomAir)
+				{
+					t.variationX = 6;
+					t.variationY = 2;
+				}
+				else
+				{
+					t.variationX = 1;
+					t.variationY = 1;
+				}
+			}
+			
 		}
+
+	
+
 	}
 
 	
